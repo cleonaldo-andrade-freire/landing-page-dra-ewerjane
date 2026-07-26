8bcd336399c5ebc7a87367566e4fdaddaf7505f5
cleonaldo-andrade-freire <cleonaldo@gmail.com>
2026-07-26 09:55:42 -0300

    Update README.md

--- check_images.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('index.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-for match in re.finditer(r'<img src=\"(data:image[^\"]+)\" alt=\"([^\"]+)\"', content):
-    b64 = match.group(1)
-    alt = match.group(2)
-    print(f'Alt: {alt} - Start: {b64[:40]}... End: {b64[-40:]} - Len: {len(b64)}')
